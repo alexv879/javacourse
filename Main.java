e1fc4a2a8aff045d72c25926ab6a7909c81e1afb
alexv879 <115034995+alexv879@users.noreply.github.com>
2024-08-30 06:23:25 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
-class Main2 {
-        public static void main(String[] args) {
-            byte zero = 0;
-            short a = 3;
-            int b = 1;
-            char d;
-            d = ' ';
-            float e = 2.0f;
-            boolean f = true;
-            String output = "H" + a + b + b + zero +" "+ "w" + zero + "r" + b + "d" +" "+ e +" "+ f;
-            System.out.println(output);
-        }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
