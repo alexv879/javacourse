39c64b5543b4cb93197681511ecdc8f257533ebf
alexv879 <115034995+alexv879@users.noreply.github.com>
2024-08-30 06:23:55 +0100

    Add files via upload

---
+++ VariableWeUse.java
@@ -0,0 +1,10 @@
+public class VariableWeUse {
+    public static void main(String[]args){
+        String petType = "FatCat";
+                String petName ="Catimus Maximus";
+        int weight = 19;
+        System.out.println("The pet type is " +petType);
+        System.out.println("The pet's name is " +petName);
+        System.out.println("The pet's weight is " +weight);
+    }
+}
