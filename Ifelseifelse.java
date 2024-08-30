fc478077689e29b3758e8f4fbbaa9b1edc48db99
alexv879 <115034995+alexv879@users.noreply.github.com>
2024-08-30 06:21:12 +0100

    Add files via upload

---
+++ Ifelseifelse.java
@@ -0,0 +1,14 @@
+public class Ifelseifelse {
+    public static void main(String[] args) {
+        int number = 4;
+        if (number == 1) {
+            System.out.println("The number is one.");
+        } else if (number == 2) {
+            System.out.println("The number is two.");
+        } else if (number == 3) {
+            System.out.println("The number is three.");
+        } else {
+            System.out.println("Quite a lot!");
+        }
+    }
+}
